f5834959d6d97172ead614ae4f615137d30d6334
Filip Sprostran <filipspro@gmail.com>
2024-02-06 23:34:47 +0100

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 export const geoApiOptions = {
 	method: 'GET',
 	headers: {
-		'X-RapidAPI-Key': '4e9f614b45msh090b09594a5721bp1c6738jsn0b4133a4705a',
+		'X-RapidAPI-Key': // Enter your rapidapi key,
 		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
 	}
 };
@@ -9,4 +9,4 @@ export const geoApiOptions = {
 export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
 
 export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
-export const WEATHER_API_KEY = "1d3d89ca3d03528fcaa8c5fb0bd5619a";
+export const WEATHER_API_KEY = // Enter your openweather key;
