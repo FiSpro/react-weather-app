9069a9ae635ab70d1cad5172df1c8e0b9a58b8a8
FiSpro <filipspro@gmail.com>
2024-02-06 02:48:24 +0100

    Get data for current weather

---
+++ src/api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4e9f614b45msh090b09594a5721bp1c6738jsn0b4133a4705a',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL = 'https://wft-geo-db.p.rapidapi.com/v1/geo';
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "1d3d89ca3d03528fcaa8c5fb0bd5619a";
